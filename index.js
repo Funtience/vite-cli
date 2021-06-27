4084b0ef3d659245c71876d3bd0a2db10b4708a5
Funtience <769558413@qq.com>
2021-06-27 20:38:24 +0800

    修改第三方模块路径

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const send = require('koa-send')
 
 const app = new Koa()
 
+const streamToString = (stream) =>
+  new Promise((resolve, reject) => {
+    let chunks = []
+    stream.on('data', (chunk) => chunks.push(chunk))
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')))
+    stream.on('error', reject)
+  })
+
 // 1. 静态文件服务器
 app.use(async (ctx, next) => {
   await send(ctx, ctx.path, { root: process.cwd(), index: 'index.html' })
+  await next()
+})
+
+// 2. 修改第三方模块的路径
+app.use(async (ctx, next) => {
+  if (ctx.type === 'application/javascript') {
+    const contents = await streamToString(ctx.body)
+    ctx.body = contents.replace(/(from\s+['"])(?![\.\/]))/g, '$1/@modules/')
+  }
   await next()
 })
 
